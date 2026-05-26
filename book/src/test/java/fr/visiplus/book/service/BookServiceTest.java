5c499581527e295dd433fbe10538614b281436bb
Visiplus <formation@visiplus.fr>
2026-05-26 15:30:44 +0200

    TI BookService

--- book/src/test/java/fr/visiplus/book/service/BookServiceTest.java
+++ book/src/test/java/fr/visiplus/book/service/BookServiceTest.java
@@ -1,13 +1,11 @@
 package fr.visiplus.book.service;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -17,20 +15,18 @@ import fr.visiplus.book.entity.Reservation;
 
 @SpringBootTest
 public class BookServiceTest {
-
+	
 	@MockitoBean
 	private ReservationService reservationService;
 	
-	@Autowired
-	private BookService bookService;
-	
 	@Test
-	public void testIsNotGetLast21Days() {
+	public void testIsNotGet21Days() {
+		
 		//Arrange
 		Book book = new Book();
 		book.setId(Long.valueOf(1));
 		book.setName("TestBook");
-		book.setDescription("Not relevant");
+		book.setDescription("Description Book");
 		book.setStatus(BookStatus.BOOKED);
 		
 		Reservation reservation = new Reservation();
@@ -40,38 +36,16 @@ public class BookServiceTest {
 		
 		book.setReservation(reservation);
 		
+		BookService bookService = new BookService(reservationService, null, null);
 		Mockito.when(reservationService.isReservationMoreThan(reservation, 21)).thenReturn(true);
 		
 		//Act
-		boolean result = bookService.isNotGetLast21Days(book);		
+		boolean result = bookService.isNotGetLast21Days(book);
 		
 		//Assert
-		assertTrue(result);		
-        Mockito.verify(reservationService).isReservationMoreThan(reservation, 21);
-
+		assertTrue(result);
+		Mockito.verify(reservationService).isReservationMoreThan(reservation, 21);
+		
 	}
-	
-	@Test
-	public void testIsNotGetLast21DaysBookNotBooked() {
-		//Arrange
-		Book book = new Book();
-		book.setId(Long.valueOf(1));
-		book.setName("TestBook");
-		book.setDescription("Not relevant");
-		book.setStatus(BookStatus.AVAILABLE);
-		
-		Reservation reservation = new Reservation();
-		reservation.setId(Long.valueOf(1));
-		reservation.setBook(book);
-		reservation.setDateResa(LocalDate.now());
-		
-		book.setReservation(reservation);
-				
-		//Act
-		boolean result = bookService.isNotGetLast21Days(book);		
-		
-		//Assert
-		assertFalse(result);
-  	}
-	
+
 }
