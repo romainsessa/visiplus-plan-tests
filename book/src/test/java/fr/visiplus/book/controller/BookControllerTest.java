dcb0b1552b54cd5aa666f4af0120b4a17f0a306c
Visiplus <formation@visiplus.fr>
2026-05-26 15:48:01 +0200

    test update

--- book/src/test/java/fr/visiplus/book/controller/BookControllerTest.java
+++ book/src/test/java/fr/visiplus/book/controller/BookControllerTest.java
@@ -1,34 +1,36 @@
 package fr.visiplus.book.controller;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class BookControllerTest {
 	
 	@Autowired
-	protected MockMvc mockMvc;
+	private MockMvc mockMvc;
 	
-
 	@Test
 	public void testGetAllBook() throws Exception {
-		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/book"));
 		
-		resultActions = resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+		//Arrange
+		String endpointUrl = "/book";
+		int expectedNumberBook = 4;
 		
-		resultActions.andExpect(jsonPath("$",hasSize(4)));	
+		//Act
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(endpointUrl));
+		
+		//Assert
+		resultActions.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+		resultActions.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(expectedNumberBook)));
+		
 	}
-
 
 }
--- book/src/test/java/fr/visiplus/book/service/ReservationServiceTest.java
+++ book/src/test/java/fr/visiplus/book/service/ReservationServiceTest.java
@@ -5,32 +5,34 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+
 import fr.visiplus.book.entity.Book;
 import fr.visiplus.book.entity.BookStatus;
 import fr.visiplus.book.entity.Reservation;
 
 public class ReservationServiceTest {
-
-	ReservationService reservationService = new ReservationService(null, null, null);
-
+	
 	@Test
-	public void testisReservationMoreThan() {
-		// Arrange
+	public void testIsReservationMoreThan() {
+		
+		//Arrange
+		ReservationService reservationService = new ReservationService(null, null, null);
+		
 		Book book = new Book();
 		book.setId(Long.valueOf(1));
 		book.setName("TestBook");
-		book.setDescription("Not relevant");
+		book.setDescription("Description Book");
 		book.setStatus(BookStatus.BOOKED);
-
+		
 		Reservation reservation = new Reservation();
 		reservation.setId(Long.valueOf(1));
 		reservation.setBook(book);
 		reservation.setDateResa(LocalDate.now());
-
-		// Act
+		
+		//Act
 		boolean result = reservationService.isReservationMoreThan(reservation, 1);
-
-		// Assert
+		
+		//Assert
 		assertFalse(result);
 	}
 
