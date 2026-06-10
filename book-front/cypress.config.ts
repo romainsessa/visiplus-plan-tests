fb8d34b14f927b6bbcc7fdd351e3876932b06fa2
romainsessa <romainsessa@gmail.com>
2026-06-10 22:31:08 +0200

    e2e

---
+++ book-front/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:4200',
+    supportFile: 'cypress/support/e2e.ts'
+  },
+});
---
+++ book-front/cypress/e2e/book.cy.ts
@@ -0,0 +1,34 @@
+describe('Book app', () => {
+
+  it('should display books', () => {
+    cy.visit('/');
+
+    cy.contains('Book list');
+
+    cy.get('.book-card').should('exist').and('have.length', 4);
+  });
+
+});
+
+it('should reserve an available book', () => {
+
+  cy.visit('/');
+
+  cy.get('.book-card')
+    .contains('.book-id', '3')   
+    .parents('.book-card')              
+    .as('availableBook');               
+
+  cy.get('@availableBook')
+    .contains('Reserve')
+    .should('exist');
+
+  cy.get('@availableBook')
+    .contains('Reserve')
+    .click();
+
+  cy.get('@availableBook')
+    .find('.status')
+    .should('contain', 'BOOKED');
+
+});
